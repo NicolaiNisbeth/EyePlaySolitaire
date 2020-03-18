31f72fd4b4cbe2a1465ca72102555e5513b03953
jdkramhoft <jdkramhoft@gmail.com>
2020-03-18 11:33:46 +0100

    RandomAgent implementation Defined singular State method

--- src/ai/State.java
+++ src/ai/State.java
@@ -1,4 +1,14 @@
 package ai;
 
+import ai.action.Action;
+
+import java.util.List;
+
 public class State {
+
+    public List<Action> getActions(){
+        return null;
+    }
+
+
 }
--- src/ai/agent/RandomAgent.java
+++ src/ai/agent/RandomAgent.java
@@ -1,4 +1,23 @@
 package ai.agent;
 
-public class RandomAgent {
+import ai.State;
+import ai.action.Action;
+import ai.heuristic.Heuristic;
+
+import java.util.List;
+
+public class RandomAgent implements Agent {
+
+    @Override
+    public Action getAction(State state) {
+        List<Action> actionList = state.getActions();
+        int count = actionList.size();
+        int choice = (int) (Math.random() * count);
+        return actionList.get(choice);
+    }
+
+    @Override
+    public void setHeuristic(Heuristic heuristic) {
+
+    }
 }
