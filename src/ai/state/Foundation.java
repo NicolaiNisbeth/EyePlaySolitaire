2e23800b444208a08231bc6db9ab37d2e20f0e40
NicolaiNisbeth <s175565@student.dtu.dk>
2020-03-19 00:03:45 +0100

    Foundation implemented

--- src/ai/state/Foundation.java
+++ src/ai/state/Foundation.java
@@ -1,4 +1,30 @@
 package ai.state;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Foundation {
+    private List<Stack<Card>> stacks = new ArrayList<>();
+    private int sum;
+
+    public Foundation(){
+        stacks.add(new Stack<>());
+        stacks.add(new Stack<>());
+        stacks.add(new Stack<>());
+        stacks.add(new Stack<>());
+    }
+
+    public void add(Card card){
+        sum += card.getValue();
+        stacks.get(card.getSuit()).add(card);
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public int getLargestDifference(){
+        return -1;
+    }
 }
