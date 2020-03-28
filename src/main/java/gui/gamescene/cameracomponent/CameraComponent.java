45812dad9485ad4387dfa87f771a3acab653d2e4
maltebp <maltebp94@gmail.com>
2020-03-28 09:50:57 +0100

    gui: Added CameraComponent package

---
+++ src/main/java/gui/gamescene/cameracomponent/CameraComponent.java
@@ -0,0 +1,17 @@
+package gui.gamescene.cameracomponent;
+
+import gui.gamescene.IComponent;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+
+public class CameraComponent implements IComponent {
+
+    public void updateImage(Image image){
+
+    }
+
+    @Override
+    public Node getNode() {
+        return null;
+    }
+}
