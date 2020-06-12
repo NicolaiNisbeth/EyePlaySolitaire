4e567920612421dfa1df574778475d063cf9267b
NicolaiNisbeth <s175565@student.dtu.dk>
2020-06-12 12:59:58 +0200

    cycle

--- src/main/java/ai/heuristic/LessUnknowns.java
+++ src/main/java/ai/heuristic/LessUnknowns.java
@@ -3,6 +3,8 @@ package ai.heuristic;
 import ai.state.State;
 
 public class LessUnknowns implements Heuristic {
+    // TODO: Expose hidden cards. If you have a choice from several possible moves that expose hidden cards, choose column with the largest number of hidden cards.
+
     @Override
     public double evaluate(State state) {
         int maxUnknowns = 21;
