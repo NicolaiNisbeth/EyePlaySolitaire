a956e11b12757c43fb63e40d15cf564b9e805760
maaaaarinaaaaa <marina@mallingnet.dk>
2020-06-20 11:39:43 +0200

    Border glow added to CardPane

--- src/main/java/gui/gamescene/gamecomponent/CardPane.java
+++ src/main/java/gui/gamescene/gamecomponent/CardPane.java
@@ -12,16 +12,39 @@ import javafx.scene.paint.Color;
  * of a dynamic card image. */
 class CardPane extends Pane {
 
-    CardPane(Image cardImage){
+    static int counter = 0;
+
+    CardPane(Image cardImage) {
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(cardImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
         setBackground(background);
 
         // Add card drop shadow
-        DropShadow shadow = new DropShadow(2, new Color(0,0,0,0.5));
+        DropShadow shadow = new DropShadow(2, new Color(0, 0, 0, 0.5));
         shadow.setWidth(0);
         shadow.setOffsetY(-2);
         setEffect(shadow);
+
+        borderGlow();
+    }
+
+    public void borderGlow() {
+        /**https://blog.idrsolutions.com/2014/02/tutorial-create-border-glow-effect-javafx/ **/
+        DropShadow borderGlow = new DropShadow();
+        int depth = 100; //Setting the uniform variable for the glow width and height
+        borderGlow.setOffsetY(0f);
+        borderGlow.setOffsetX(0f);
+        borderGlow.setWidth(depth);
+        borderGlow.setHeight(depth);
+        borderGlow.setSpread(0.5);
+
+        if (counter % 2 == 0) {
+            borderGlow.setColor(Color.RED);
+        } else if(counter % 2 != 0) {
+            borderGlow.setColor(Color.WHITE);
+        }
+
+        counter++;
     }
 }
